35c97921d1464f25ef0402179b90983ee03e9481
Joselin <127jramirez@frhsd.com>
2025-12-15 14:03:10 -0500

    12/15 natasha loves pizza

---
+++ script.js
@@ -0,0 +1,9 @@
+let sumbit = document.getElementById("button");
+console.log(button);
+
+sumbit.addEventListener('click',getWords);
+
+function getWords(){
+console.log(sumbit);
+}
+var words = document.querySelectorAll('input[type=text]');
