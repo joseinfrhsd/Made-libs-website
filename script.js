32c54ae9a294aa5cd9148b99fb2607f6aa5a42a0
Joselin <127jramirez@frhsd.com>
2025-12-18 12:53:53 -0500

    12/18

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let sumbit = document.getElementById("button");
-console.log(button);
+// console.log(button);
 
 sumbit.addEventListener('click',getWords);
 
 function getWords(){
-console.log(sumbit);
+    var words = document.querySelectorAll('input[type=text]');
+    var placeholders =document.querySelectorAll(".userInput");
+    placeholders.forEach((placeholder, index) =>  {
+        placeholder.innerHTML=words[index].value;
+    });
+
+// Show hidden story
+document.getElementById("story-screen").style.display="block"
 }
-var words = document.querySelectorAll('input[type=text]');
